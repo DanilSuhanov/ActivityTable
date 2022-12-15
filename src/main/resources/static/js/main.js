c3d61e40cef63a1a1f7eb2f1597904caff952bff
Данил <Muravey7511@mail.ru>
2022-12-16 00:04:00 +0300

    16.12 - header done

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const userFetch = {
+    head: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Referer': null
+    },
+    findUserByUsername: async () => await fetch(`api/user`)
+}
+
+async function header() {
+    let temp = '';
+    const info = document.querySelector('#user-info');
+    await userFetch.findUserByUsername()
+        .then(res => res.json())
+        .then(user => {
+            temp += `
+             <span id="user-info" class="navbar-brand" style="color:#f5f5f5">
+               ${user.username} with roles <span>${user.roles.map(role => " " + role.authority.slice(5))}</span>
+            </span>
+            `;
+        });
+    info.innerHTML = temp;
+}
+
+header();
