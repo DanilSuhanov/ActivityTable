c98cf72124fb580f6e57deccf25cc2279f818451
Данил <Muravey7511@mail.ru>
2023-02-16 12:13:50 +0300

    mini bug fix

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -14,7 +14,17 @@ async function switchMenu() {
         update(attributeClass, passiveValue);
         menuProfile.setAttribute(attributeClass, activeValue);
 
-        createCard(colContent);
+        colContent.innerHTML = `<div class="card">
+                    <div class="row g-0">
+                        <div class="col">
+                            <div class="card-body">
+                                <h5 class="card-title" id="cardUsername"></h5>
+                                <p class="card-text" id="cardSubordinates"></p>
+                                <p class="card-text"><button type="button" class="btn btn-primary">Изменить ифнормацию</button></p>
+                            </div>
+                        </div>
+                    </div>
+                  </div>`;
         profileLoad();
     }
 
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,17 +1,3 @@
-function createCard(colContent) {
-    colContent.innerHTML = `<div class="card">
-                    <div class="row g-0">
-                        <div class="col">
-                            <div class="card-body">
-                                <h5 class="card-title" id="cardUsername"></h5>
-                                <p class="card-text" id="cardSubordinates"></p>
-                                <p class="card-text"><button type="button" class="btn btn-primary">Изменить ифнормацию</button></p>
-                            </div>
-                        </div>
-                    </div>
-                  </div>`;
-}
-
 async function profileLoad() {
     await fetch(`api/user`)
         .then(res => res.json())
